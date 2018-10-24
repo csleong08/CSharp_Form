adcb78194d805e2646e231be70de58dce36b87c6
Chee Sheng Leong <csleong@Chees-MacBook-Pro.local>
2018-10-24 13:33:16 -0700

    first push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Form.Models;
+
+namespace Form.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(User myUser)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Success", myUser);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
